8ac81067c62eea0155a859030a9406b454c1badb
Seth <seth.d.garrett@gmail.com>
2019-04-18 00:23:17 -0700

    Seth created new file for testing battles

---
+++ NinjaWorld/src/Story4.java
@@ -0,0 +1,23 @@
+
+public class Story4 implements Story {
+
+	@Override
+	public void start_() {
+		// TESTING BATTLES
+		System1 s = new System1();
+		s.out("Story4 begin");
+		
+		Character testC = new Character();
+		testC.stats_.loadCreatureStats("clawed", 4);
+		testC.printStatsFormal();
+		
+		Creature monster = new Creature();
+		monster.stats_.loadCreatureStats("flying", 2);
+		monster.printStatsFormal();
+		
+		s.out("Enter integer to quit");
+		s.getInt();
+		System.exit(0);
+	}
+
+}//end Story4
